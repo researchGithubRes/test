d7bcbf7582d6ceebecb9f2c40dfa1060e8f8b931
researchGithubRes <55343486+researchGithubRes@users.noreply.github.com>
2019-09-21 10:06:42 +0530

    file used used for edu purposes

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+//echo "hi";
+// checking file  Upload formats
+$file='jpg';
+
+$ImageFileType = [
+     'jpeg',
+     'jpg',
+     'png'
+    ];
+
+   
+    
+
+
+    // if (!in_array($file,$ImageFileType)) {
+       
+    //  echo " not matched";
+    // }
+    // else {
+    //   echo "  matched";
+    // }
+    $art="my_blog";
+    $art1="my_blog1";
+//var_dump(compact('articles'));
+//array(1) { ["articles"]=> string(7) "my_blog" }
+
+//var_dump(compact($articles));
+//array(0) { }
+
+//echo "<pre>";
+//var_dump(compact('art','art1'));
+
+$items = array(1, 2, 3, 4, 5);
+__::each($items, function($item) { print $item; });
+
+
+//echo ['Datasources']['default']['className']
+// echo $test ['Datasources']['default']['className'];
+
+// https://www.geodata.solutions/?chronoform=listbuilder&event=submit
+
+?>
+
